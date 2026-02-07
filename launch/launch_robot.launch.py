788fda955feaf0036b29c73e73d2e4b4b7b73483
Samuel <stri0019@student.monash.edu>
2026-02-07 21:40:38 +1100

    Added a launch_robot.launch.py for launching all relevant nodes for hardware (Moving actual robot)

---
+++ launch/launch_robot.launch.py
@@ -0,0 +1,103 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction, RegisterEventHandler
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
+from launch.event_handlers import OnProcessStart
+from launch_ros.parameter_descriptions import ParameterValue
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+# import os
+# from ament_index_python.packages import get_package_share_directory
+# from launch import LaunchDescription
+# from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, RegisterEventHandler, SetEnvironmentVariable
+# from launch.launch_description_sources import PythonLaunchDescriptionSource
+# from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
+# from launch.event_handlers import OnProcessExit
+# from launch_ros.actions import Node
+# from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    # Defining input parameters
+
+    # -------------------------------
+    # 1. Robot description + TF
+    # -------------------------------
+    use_sim_time = LaunchConfiguration('use_sim_time', default = 'true')
+    package_name = 'esda_simulation_2025'
+
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
+    )
+
+    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+
+    # controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers_2wd.yaml')
+
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare(package_name),
+            'config',
+            'my_controllers_2wd.yaml',
+        ]
+    )
+
+    controller_manager = Node(
+        package = "controller_manager",
+        executable = "ros2_control_node",
+        parameters=[{'robot_description': robot_description},
+                    robot_controllers]
+    )
+
+    delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
+
+    # --- 5) Load and start your controllers ---
+    # Controller manager spawners
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+        output='screen'
+    )
+
+    delayed_joint_broad_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[joint_broad_spawner],
+        )
+    )
+
+    diff_drive_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[
+            'diff_drive_base_controller',
+            '--param-file', robot_controllers,
+            #'--ros-args', '-r', '/diff_drive_base_controller/cmd_vel:=/cmd_vel'
+        ],
+        output='screen'
+    )
+
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[diff_drive_spawner],
+        )
+    )
+
+    # Launch them all!
+    return LaunchDescription([
+        rsp,
+        # joystick,
+        # twist_mux,
+        delayed_controller_manager,
+        delayed_diff_drive_spawner,
+        delayed_joint_broad_spawner
+    ])
